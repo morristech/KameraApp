2dafdf644937a32814fa2d1edfe747233218e83f
Henrique Menezes <Henriquespmenezes@outlook.com>
2018-04-06 17:05:57 -0300

    -

--- app/src/main/java/com/example/henrique/kamera/CameraVisual.java
+++ app/src/main/java/com/example/henrique/kamera/CameraVisual.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
 import java.io.IOException;
 
 public class CameraVisual extends SurfaceView implements SurfaceHolder.Callback{
@@ -15,19 +14,16 @@ public class CameraVisual extends SurfaceView implements SurfaceHolder.Callback{
         super(context);
         mCamera = camera;
         getHolder().addCallback(this);
-
     }
-
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
-        }catch (IOException e ){
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
-
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
